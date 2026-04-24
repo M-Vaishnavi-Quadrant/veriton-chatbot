99aeffb27841872cffe264cb082025915799e508
M-Vaishnavi-Quadrant <Vaishnavi.M@quadranttechnologies.com>
2026-04-24 12:42:17 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "API is working!"}
+
+@app.get("/test")
+def test_api():
+    return {
+        "status": "success",
+        "data": "Test endpoint is working fine"
+    }
